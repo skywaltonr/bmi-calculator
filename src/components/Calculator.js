8c22807f9fc6968631d5de711e90b50e7f63c761
Ryan Walton <rwalton2@sheffield.ac.uk>
2023-03-18 13:57:22 +0000

    basic structure

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,8 +1,23 @@
 const Calculator = () =>{
     return(
-        <div className='container'>
+        <div className='p-3 border-2 rounded-md max-w-lg mx-auto my-3'>
             <div>
-                <h1>BMI Calculator</h1>
+                <h1 className="mx-auto w-fit">BMI Calculator</h1>
+                <form className="flex flex-col">
+                    <label className="font-semibold mb-1">
+                        Height
+                    </label>
+                    <input type="text" className="p-1 border-2 rounded-md mb-2"/>
+                    <label className="font-semibold mb-1">
+                        Weight
+                    </label>
+                    <input type="text" className="p-1 border-2 rounded-md mb-2"/>
+                    <label className="font-semibold mb-1">
+                        Age
+                    </label>
+                    <input type="text" className="p-1 border-2 rounded-md mb-2"/>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         </div>
     )
